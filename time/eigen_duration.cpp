5225a812655f019118204d06eb39000e88c468e8
Juan Esteban Agudelo Ortiz <juagudeloo@unal.edu.co>
2021-04-22 15:42:03 -0500

    chrono

--- time/eigen_duration.cpp
+++ time/eigen_duration.cpp
@@ -1,3 +1,11 @@
 #include<iostream>
 #include<cmath>
-#
+
+int main()
+{
+   std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    start = std::chrono::system_clock::now();
+    //std::cout << "f(42) = " << fibonacci(42) << '\n';
+    end = std::chrono::system_clock::now();
+}
