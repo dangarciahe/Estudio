57b273eab273bbb3419bfc2f1942482995ff181f
daniel <dangarciahe>
2021-04-22 17:17:08 -0500

    lala

--- time/eigen_duration.cpp
+++ time/eigen_duration.cpp
@@ -1,11 +1,37 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <Eigen/Eigenvalues>
+//#include <eigen3/Eigen/Dense>
 
-int main()
+
+typedef Eigen::EigenSolver<Eigen::Matrix<double,-1,-1>> solver; 
+
+solver eigen_v(const Eigen::MatrixXd &m);
+  
+int main(int argc, char **argv)
 {
-   std::chrono::time_point<std::chrono::system_clock> start, end;
+  
+  std::chrono::time_point<std::chrono::system_clock> start, end;
 
-    start = std::chrono::system_clock::now();
-    //std::cout << "f(42) = " << fibonacci(42) << '\n';
-    end = std::chrono::system_clock::now();
+  int N = std::atoi(argv[1]);
+  Eigen::MatrixXd m = Eigen::MatrixXd::Random(N,N);
+  // Eigen::MatrixXd ones = Eigen::MatrixXd::Ones (N,N); //linea desechada
+
+  start = std::chrono::system_clock::now(); //start timer
+  //funcion a medir
+  auto sol = eigen_v(m);
+  end = std::chrono::system_clock::now(); //end timer
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  
+  std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+  //std::cout << sol.eigenvectors().col(0)<< std::endl;
+  
+}
+
+
+solver eigen_v(const Eigen::MatrixXd &m){
+  Eigen::EigenSolver<Eigen::MatrixXd> sol(m, false);
+  
+  return sol;
 }
