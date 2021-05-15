bc2adf6659a6aafce0fd62fd02b0eb97d4911250
dangarciahe <dangarciahe@unal.edu.co>
2021-05-15 12:36:08 -0500

    Primer commit de integral paralela

---
+++ parallel/Crackencio/integral.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <omp.h>
+
+double f(double x);
+
+int main (){
+  //inputs
+  const double a = 0;
+  const double b = 10;
+  
+
+  return 0;
+}
+
+
+double f (double x){
+  double valor = x*x;
+  return valor;
+}
